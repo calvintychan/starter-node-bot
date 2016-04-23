65a9a4c4a790ffb86a188092feb0f2e72c67b483
Calvin Chan <calvin@spright.com>
2016-04-22 23:54:44 -0700

    Move everything to index.js

--- index.js
+++ index.js
@@ -66,3 +66,63 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
+
+// Facebook Messenger
+var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
+var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN;
+var port = process.env.PORT;
+
+if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required');
+if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required');
+if (!port) throw new ERROR('PORT is required');
+
+var controller = Botkit.facebookbot({
+  access_token: accessToken,
+  verify_token: verifyToken
+});
+
+var bot = controller.spawn();
+
+controller.startServer(port, function (err, server) {
+  if (err) return console.log(err);
+  controller.createWebhookEndpoints(server, bot, function () {
+    console.log('Server started!');
+  });
+});
+
+controller.hears(['hello', 'hi'], 'message_received', function (bot, message) {
+  bot.reply(message, 'Hello!');
+  bot.reply(message, 'Good day!');
+  bot.reply(message, {
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'button',
+        text: 'Ready to be productive?',
+        buttons: [
+          {
+            type: 'postback',
+            title: 'Yes',
+            payload: 'yes'
+          },
+          {
+            type: 'postback',
+            title: 'No',
+            payload: 'no'
+          }
+        ]
+      }
+    }
+  });
+});
+
+controller.on('facebook_postback', function (bot, message) {
+  switch (message.payload) {
+    case 'yes':
+      bot.reply(message, 'Let\'s get you started!');
+      break;
+    case 'no':
+      bot.reply(message, 'Okay, maybe tomorrow then.');
+      break;
+  }
+});
